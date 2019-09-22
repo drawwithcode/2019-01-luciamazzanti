8ae8003ebfa1ec621db20cf8b39daad4b2c38419
Lucia Mazanti <luciamazzanti4@gmail.com>
2019-09-22 18:20:01 +0200

    Es 1

--- sketch.js
+++ sketch.js
@@ -4,8 +4,21 @@ function preload(){
 
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(41,56,88);
+  angleMode(DEGREES);
+  frameRate(12);
 }
 
 function draw() {
   // put drawing code here
+  translate(width/2, height/2);
+
+  strokeWeight(2);
+  stroke(lerpColor(color(221, 93, 0),color(247, 188, 19),frameCount/60));
+  line(width/6, 0, cos(frameCount*6) * width/6, sin(frameCount*6) * width/6);
+
+  if (frameCount == 60) {
+    noLoop();
+  }
 }
