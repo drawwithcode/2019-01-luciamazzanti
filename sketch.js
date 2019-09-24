abac5d9b049a93d565238bd4cd6567d3c22f93de
Lucia Mazanti <luciamazzanti4@gmail.com>
2019-09-25 01:11:27 +0200

    Changes

--- sketch.js
+++ sketch.js
@@ -12,13 +12,36 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  translate(width/2, height/2);
 
-  strokeWeight(2);
-  stroke(lerpColor(color(221, 93, 0),color(247, 188, 19),frameCount/60));
-  line(width/6, 0, cos(frameCount*6) * width/6, sin(frameCount*6) * width/6);
+if (frameCount < 60) {
+  //yellow/orange sun
+   translate(width/2, height/2);
+   stroke(lerpColor(color(221, 93, 0),color(247, 188, 19),frameCount/60));
+   line(width/6, 0, cos(frameCount*6) * width/6, sin(frameCount*6) * width/6);
+ } else if (frameCount > 60 & frameCount < 120) {
+   //white pearl
+   translate(width/2, height/2);
+   stroke("white");
+   noFill();
+   triangle(width/6, 0, 0, height/6, cos(frameCount*6) * width/6, sin(frameCount*6) * width/6);
+ } else if (frameCount > 120) {
+   //pink shell
+   push();
+   translate(width/2, height*5/6);
+   stroke("pink");
+   strokeWeight(1);
+   line(width/9, 0, cos(frameCount*6) * width/6, sin(frameCount*6) * width/9);
+   pop();
 
-  if (frameCount == 60) {
-    noLoop();
-  }
+   //bubbles
+   stroke("white");
+   strokeWeight(5);
+   ellipse(mouseX, mouseY, 25, 25);
+
+   //text
+   textSize(50);
+   strokeWeight(1);
+   textAlign(CENTER);
+   text('Move the mouse to create bubbles!', width/2, 70);
+ }
 }
